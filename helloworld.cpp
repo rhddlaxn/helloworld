f6f0fbd3c640dd92eaa73241238dae2197530ed5
LimHyeongMin <hymin1119@naver.com>
2019-07-26 10:10:08 +0900

    init

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+
+CONTRACT helloworld: public contract {
+public:
+using contract::contract;
+
+ACTION hi(name user) {
+require_auth(user);
+
+print("Hello, ", user);
+}
+};
